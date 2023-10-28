19e9bf9e236b45b9bf77e4e5eda347b8217e6d54
unknown <jluciferph@gmail.com>
2023-10-28 12:54:28 +0700

    first commit

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 import pg from 'pg';
 const { Pool } = pg
 
+const CONNECT = "postgresql://phhh:pZU-e3IHMu8lWgwyp9ggsw@cheeky-tapir-11314.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+
 const pool = new Pool({
-    user: "postgres",
-    password: "admin",
-    host: "https://sm-tr.onrender.com/",
-    port: "5432",
-    database: "node_postgres"
+    connectionString: CONNECT,
+    application_name: "docs_simplecrud_node-postgres"
 })
 
+const start = async () => {
+    const client = await pool.connect();
+}
 
 export default pool
--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 import express, { Router } from 'express'
 import { router } from './src/routes/userRotes.js'
+import pg from "pg"
+import cors from "cors"
+
+const Client = pg.Client
+
+
 
 const PORT = process.env.PORT || 5000
+const DATABASE_URL = "postgresql://phhh:pZU-e3IHMu8lWgwyp9ggsw@cheeky-tapir-11314.8nj.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
 
+const client = new Client(DATABASE_URL);
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/api', router)
 
 const start = async () => {
+    await client.connect();
     try {
+        const results = await client.query("SELECT NOW()");
         app.listen(PORT, () => { 
             console.log(`Server has started on PORT ${PORT}`)
         })
     } catch (error) {
         console.log(error)
+    } finally {
+        client.end();
     }
 }
 
